2ba2546f4d868917c3d06deb3630b9267a328949
lakhdari_abdallah <110565989+abdallahlakh@users.noreply.github.com>
2024-02-19 08:37:08 +0100

    Update admin.py

--- mouhami_api/admin.py
+++ mouhami_api/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
-admin.site.register(models.Booking)
+# admin.site.register(models.Booking)
 admin.site.register(models.Language)
 admin.site.register(models.Lawyer)
-admin.site.register(models.Review)
 admin.site.register(models.Specialities)
