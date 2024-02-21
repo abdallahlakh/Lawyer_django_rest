5d715bd49124fa2667c64ab7804de234581cea54
lakhdari_abdallah <110565989+abdallahlakh@users.noreply.github.com>
2024-02-21 23:16:47 +0100

    Update urls.py

--- mouhami_api/urls.py
+++ mouhami_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import searchLawyer,lawyerData
+from .views import searchLawyer,lawyerData,lawyerDetail
 
 
 urlpatterns = [
     path('import-lawyers/',lawyerData),
     path('search-lawyers',searchLawyer),
+    path('lawyer-detail/<int:id>/', lawyerDetail),
 ]
